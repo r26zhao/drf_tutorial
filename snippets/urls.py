61e007714ae1388ade3f2fd879cd80d9d4ce0c88
Aaron Zhao <rudy710@qq.com>
2018-02-10 21:59:14 +0800

    authentication and permission

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,6 +4,8 @@ from snippets import views
 app_name = 'snippets'
 
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>\d+)/$', views.snippet_detail),
+    url(r'^snippets/$', views.SnippetsList.as_view()),
+    url(r'^snippets/(?P<pk>\d+)/$', views.SnippetDetail.as_view()),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>\d+)/$', views.UserDetail.as_view()),
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -1,50 +1,33 @@
-from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import generics
+from rest_framework import permissions
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+from django.contrib.auth.models import User
+from snippets.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
-@csrf_exempt
-def snippet_list(request):
-    """
-    List all code snippets, or create a new snippet
-    :param request:
-    :return:
-    """
-    if request.method == 'GET':
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+class SnippetsList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
-@csrf_exempt
-def snippet_detail(request, pk):
-    '''
-    retrieve, update or delete a code snippet
-    :param request:
-    :param pk:
-    :return:
-    '''
-    snippet = get_object_or_404(Snippet, pk=pk)
-    if request.method == 'GET':
-        serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-    elif request.method == 'DELETE':
-        snippet.delete()
-        return HttpResponse(status=204)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
